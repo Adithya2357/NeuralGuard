a8986f61694c8414c8c812dd7d309a015f24c713
Adithya Chiluka <98212055+Adithya2357@users.noreply.github.com>
2024-11-04 06:36:32 -0500

    Update detection_consumer.py

--- real_time_detection/detection_consumer.py
+++ real_time_detection/detection_consumer.py
@@ -2,20 +2,18 @@ from kafka import KafkaConsumer
 import json
 import numpy as np
 import joblib
-from log_to_elasticsearch import log_threat  # Importing the logging function
+from log_to_elasticsearch import log_threat
 
 print("Loading model...")  # Debugging line
 model = joblib.load('/home/kali/Downloads/NeuralGuard/model/saved_models/threat_detection_model.pkl')
-print("Model loaded successfully.")  # Debugging line
-
-# Set up the Kafka consumer
-print("Setting up Kafka consumer...")  # Debugging line
+print("Model loaded successfully.")  
+print("Setting up Kafka consumer...") 
 consumer = KafkaConsumer(
     'network-traffic',
     bootstrap_servers='localhost:9092',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
-print("Kafka consumer set up. Waiting for messages...")  # Debugging line
+print("Kafka consumer set up. Waiting for messages...")  
 
 THREAT_THRESHOLD = 0.5
 
@@ -26,16 +24,15 @@ def process_data(data):
 
 def detect_threats():
     for message in consumer:
-        print("Received message:", message.value)  # Debugging line
+        print("Received message:", message.value) 
         data = message.value
         input_data = process_data(data)
-        threat_score = model.predict_proba(input_data)[0][1]  # Probability of being a threat
+        threat_score = model.predict_proba(input_data)[0][1]  
 
-        print(f"Threat score: {threat_score}")  # Debugging line
-
+        print(f"Threat score: {threat_score}")  
         if threat_score >= THREAT_THRESHOLD:
             print("Threat Detected!")
-            log_threat(data, threat_score)  # Log to Elasticsearch if it's a threat
+            log_threat(data, threat_score) 
 
 if __name__ == "__main__":
     detect_threats()
